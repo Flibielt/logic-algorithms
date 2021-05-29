b9d0b085000046753da99efd8793d7da01f572f2
Csaba Hermann <hcsabi98@gmail.com>
2021-05-30 01:13:19 +0200

    Continued implementing the algorithm I

--- algorithm_i.py
+++ algorithm_i.py
@@ -1,3 +1,6 @@
+from random import randrange
+
+
 class Literal:
     def __init__(self, value):
         self.value = value
@@ -29,13 +32,50 @@ class Clause:
         return False
 
 
+def is_strictly_distinct(literals, new_literal):
+    for literal in literals:
+        if literal.value == new_literal:
+            return False
+        elif abs(literal.value) == abs(new_literal):
+            return False
+
+    return True
+
+
+def find_falsified_clause(clauses, literals):
+    for i in range(0, len(clauses)):
+        for literal in literals:
+            if not clauses[i].is_satisfied_by(literal):
+                return i
+    return -1
+
+
 def algorithm_i(clauses, n):
     print("Algorithm I")
     m = len(clauses)
+    literals = []
     d = 0
 
-    if d == n:
-        return True
+    while True:
+        # I2: Advanced
+        if d == n:
+            return True
+        else:
+            l_d = randrange(-10, 11)
+            while not is_strictly_distinct(literals, l_d):
+                l_d = randrange(-10, 11)
+            literals.append(l_d)
+            d = d + 1
+
+        # I3: Find falsified C_i
+        i = find_falsified_clause(clauses, literals)
+        while i > -1:
+            # I4: Negate l_d and search for falsified clauses
+            literals[d] = -1 * literals[d]
+            j = find_falsified_clause(clauses, literals)
+
+            if j < 0:
+                break
 
 
 def main():
